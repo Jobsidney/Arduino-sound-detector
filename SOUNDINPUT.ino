86d846091ce6e4c00912d6fb0e41e515e2b00e36
Jobsidney <70740330+Jobsidney@users.noreply.github.com>
2021-12-05 14:26:50 +0300

    Add files via upload

---
+++ SOUNDINPUT.ino
@@ -0,0 +1,24 @@
+
+const int ledpin=13;
+const int soundpin=A2;
+const int threshold=200;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+pinMode(ledpin,OUTPUT);
+pinMode(soundpin,INPUT);
+}
+
+void loop() {
+  int soundsens=analogRead(soundpin);
+  if(soundsens>=threshold){
+    digitalWrite(ledpin,HIGH);
+   delay(167i );}
+   else{
+    digitalWrite(ledpin,LOW);}
+  //continues to display light aslong as there is sound input to the mic
+
+}
